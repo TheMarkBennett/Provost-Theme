05a8da70de2e4d7d1bd41bea0ac9f7f9985f8fbb
Mark Bennett <mark.bennett@ucf.edu>
2018-12-04 14:51:27 -0500

    added initial files
    added all the initial files

---
+++ singular.php
@@ -0,0 +1,19 @@
+<?php get_header(); the_post(); ?>
+
+<article class="<?php echo $post->post_status; ?> post-list-item">
+<?php if( get_field('ucf_theme_content_layout') != "container-fluid" || get_field('ucf_theme_sidebar') != "no_sidebar"): ?>
+	<div class="<?php the_field('ucf_theme_content_layout'); ?> mt-4 mt-sm-5 mb-5 pb-sm-4">
+    <div class="row">
+  <?php endif; ?>
+      <?php
+        if(get_field('ucf_theme_sidebar') == "left_sidebar"): get_sidebar(''); endif; //adds left sidebar
+        get_template_part( 'templates/content', 'no_sidebar' ); //content
+        if(get_field('ucf_theme_sidebar') == "right_sidebar"): get_sidebar(''); endif; //right sidebar
+      ?>
+  <?php if( get_field('ucf_theme_content_layout') != "container-fluid" || get_field('ucf_theme_sidebar') != "no_sidebar"): ?>
+    </div>
+  </div>
+<?php endif; ?>
+</article>
+
+<?php get_footer(); ?>
---
+++ templates/content-no_sidebar.php
@@ -0,0 +1,12 @@
+		<?php // hooks will be coming ?>
+		<?php
+		$width = '';
+			if( get_field('ucf_theme_sidebar') != "no_sidebar" ):
+				$width = 'col-12 col-md-8';
+			endif;
+		?>
+  <div class="content-area primary <?php echo $width ?>">
+    <div class="entry-content">
+		    <?php the_content(); ?>
+    </div>
+  </div>

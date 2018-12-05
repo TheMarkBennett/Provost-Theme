b24821bcfb4bb8134787321b8e5030c0349fbc0f
Mark Bennett <mark.bennett@ucf.edu>
2018-12-05 15:00:08 -0500

    update default page

--- includes/acf/settings.php
+++ includes/acf/settings.php
@@ -24,7 +24,7 @@ acf_add_local_field_group(array(
 				'right_sidebar' => 'Right Sidebar',
 			),
 			'default_value' => array(
-				0 => 'No Sidebar',
+				0 => 'right_sidebar',
 			),
 			'allow_null' => 0,
 			'multiple' => 0,

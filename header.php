fb95f0d22a41af878b65b3926a85a6d7c72db4fb
Mark Bennett <mark.bennett@ucf.edu>
2019-01-02 11:10:11 -0500

    added header file

---
+++ header.php
@@ -0,0 +1,18 @@
+!DOCTYPE html>
+<html lang="en-us">
+	<head>
+		<?php wp_head(); ?>
+	</head>
+	<body ontouchstart <?php body_class(); ?>>
+		<a class="skip-navigation bg-complementary text-inverse box-shadow-soft" href="#content">Skip to main content</a>
+		<div id="ucfhb"></div>
+
+		<?php do_action( 'after_body_open' ); ?>
+
+		<header class="site-header">
+			<?php echo ucfwp_get_header_markup(); ?>
+		</header>
+
+		<main class="site-main">
+			<?php echo ucfwp_get_subnav_markup(); ?>
+			<div class="site-content" id="content" tabindex="-1">

e5042507e48833ade34792daf08bfdea1d316e0f
Mark Bennett <mark.bennett@ucf.edu>
2019-01-02 09:41:38 -0500

    updated side bar width

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div class="widget-area secondary col-xs-12 col-md-4">
+<div class="widget-area secondary col-xs-12 col-md-2">
   <?php if ( is_active_sidebar( 'main_sidebar' ) ) : ?>
         <div class="sidebar-main">
           <?php dynamic_sidebar( 'main_sidebar' ); ?>

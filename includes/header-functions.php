b0d1d8d64c00616590e37c1c0427b75ca9ca21a8
Mark Bennett <mark.bennett@ucf.edu>
2019-01-04 12:04:00 -0500

    breadcrums

--- includes/header-functions.php
+++ includes/header-functions.php
@@ -20,7 +20,7 @@ add_action('wp_loaded', 'remove_my_parent_theme_function');
  * @return string HTML for the page header
  **/
 
-	function ucfwp_get_header_default_markup( $obj ) {
+ function ucfwp_get_header_default_markup( $obj ) {
 		$title               = ucfwp_get_header_title( $obj );
 		$subtitle            = ucfwp_get_header_subtitle( $obj );
 		$field_id            = ucfwp_get_object_field_id( $obj );
@@ -30,10 +30,8 @@ add_action('wp_loaded', 'remove_my_parent_theme_function');
 		$h1_elem             = ( is_home() || is_front_page() ) ? 'h2' : 'h1'; // name is misleading but we need to override this elem on the homepage
 		$title_elem          = ( $h1 === 'title' ) ? $h1_elem : 'span';
 		$subtitle_elem       = ( $h1 === 'subtitle' ) ? $h1_elem : 'p';
-
 		$title_classes = 'h1 d-block mt-3 mt-sm-4 mt-md-5 mb-2 mb-md-3';
 		$subtitle_classes = 'lead mb-2 mb-md-3';
-
 		ob_start();
 	?>
 		<?php if ( !$exclude_nav ) { echo ucfwp_get_nav_markup( false ); } ?>
@@ -45,14 +43,14 @@ add_action('wp_loaded', 'remove_my_parent_theme_function');
 		?>
 		<div class="container">
 			<<?php echo $title_elem; ?> class="<?php echo $title_classes; ?>">
-			<?php if(function_exists('seopress_display_breadcrumbs')) { seopress_display_breadcrumbs(); } ?>
+				<?php if(function_exists('seopress_display_breadcrumbs')) { seopress_display_breadcrumbs(); } ?>
 				<?php echo $title; ?>
-			<?php echo $title_elem; ?>>
+			</<?php echo $title_elem; ?>>
 
 			<?php if ( $subtitle ): ?>
-				<?php echo $subtitle_elem; ?> class="<?php echo $subtitle_classes; ?>">
+				<<?php echo $subtitle_elem; ?> class="<?php echo $subtitle_classes; ?>">
 					<?php echo $subtitle; ?>
-				<?php echo $subtitle_elem; ?>
+				</<?php echo $subtitle_elem; ?>>
 			<?php endif; ?>
 		</div>
 		<?php endif; ?>

c695cb8e9205d3eb78f1ffccedccde04d7a5466b
Mark Bennett <mark.bennett@ucf.edu>
2019-01-04 12:51:35 -0500

    change marging padding

--- singular.php
+++ singular.php
@@ -2,7 +2,7 @@
 
 <article class="<?php echo $post->post_status; ?> post-list-item">
 <?php if( get_field('ucf_theme_content_layout') != "container-fluid" || get_field('ucf_theme_sidebar') != "no_sidebar"): ?>
-	<div class="<?php the_field('ucf_theme_content_layout'); ?> mt-3 mt-sm-3 mb-5 pb-sm-4">
+	<div class="<?php the_field('ucf_theme_content_layout'); ?> mt-2 mt-sm-1 mb-5 pb-sm-4">
     <div class="row">
   <?php endif; ?>
       <?php

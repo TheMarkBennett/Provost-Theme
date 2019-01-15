018498080992f449739af285a00aac9b092a9372
Mark Bennett <mark.bennett@ucf.edu>
2019-01-15 15:37:51 -0500

    version

--- archive-initiatives.php
+++ archive-initiatives.php
@@ -20,11 +20,10 @@ add_filter( 'get_the_archive_title', 'grd_custom_archive_title' );
 	 <?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
  	<?php if ( have_posts() ): ?>
  		<?php while ( have_posts() ) : the_post(); ?>
+		 <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );?>
  		<article class="<?php echo $post->post_status; ?> post-list-item mt-4 mb-4 row no-gutters " style="background-color:#f9f9f9;">
-			<div class="col-md-4">
-			<?php if ( has_post_thumbnail() ) : ?>
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?></a>
-			<?php endif; ?>
+		<div class="col-md-4"  style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat center top; background-size:cover;">
+					<div class="py-4 px-4"></div>
 		</div>
 			<div class="col-md-8 py-5 px-5 align-self-center text-center">
  			<h2 class="h3">

9487686374ce21f09387b1bd126edee1f724ade9
Mark Bennett <mark.bennett@ucf.edu>
2019-01-16 08:24:47 -0500

    breadvrumbs

--- archive-initiatives.php
+++ archive-initiatives.php
@@ -16,16 +16,16 @@ add_filter( 'get_the_archive_title', 'grd_custom_archive_title' );
  ?>
 
  <div class="container mt-4 mb-5 pb-sm-4 ">
-	 <?php if(function_exists("seopress_display_breadcrumbs")) { seopress_display_breadcrumbs(); } ?>
+	<div class="ucf-breadcrumbs mt-4">	 <?php if(function_exists("seopress_display_breadcrumbs")) { seopress_display_breadcrumbs(); } ?> </div>
 	 <?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
  	<?php if ( have_posts() ): ?>
  		<?php while ( have_posts() ) : the_post(); ?>
 		 <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );?>
  		<article class="<?php echo $post->post_status; ?> post-list-item mt-4 mb-4 row no-gutters " style="background-color:#f9f9f9;">
-		<div class="col-md-4"  style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat center top; background-size:cover;">
+		<div class="col-md-5"  style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat center top; background-size:cover;">
 					<div class="py-4 px-4"></div>
 		</div>
-			<div class="col-md-8 py-5 px-5 align-self-center text-center">
+			<div class="col-md-7 py-5 px-5 align-self-center text-center">
  			<h2 class="h3">
  				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
  			</h2>

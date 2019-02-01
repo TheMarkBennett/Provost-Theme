62fe12b8872cfcdd035e370006b5efcbdc0b5914
Mark Bennett <mark.bennett@ucf.edu>
2019-02-01 10:42:03 -0500

    add row to current title

--- archive-initiatives.php
+++ archive-initiatives.php
@@ -25,7 +25,7 @@ add_filter( 'get_the_archive_title', 'grd_custom_archive_title' );
  		<article class="<?php echo $post->post_status; ?> post-list-item mt-4 mb-4 row  ">
 		
 			<li class="pb-3 initiatives ">
- 			<h2 class="h3 text-secondary">
+ 			<h2 class="h3 text-secondary row">
  				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
  			</h2>
  			<div class="summary">
@@ -39,6 +39,8 @@ add_filter( 'get_the_archive_title', 'grd_custom_archive_title' );
  	<?php else: ?>
  		<p>No results found.</p>
  	<?php endif; ?>
+
+	 <h3 class="mt-4 heading-underline"> Completed Task Forces </h3>
  </div>
 
  <?php get_footer(); ?>

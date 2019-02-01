5b88d2f4ff9adf4d6fd40bdb8286f67dfc299f6a
Mark Bennett <mark.bennett@ucf.edu>
2019-02-01 10:36:54 -0500

    padding

--- archive-initiatives.php
+++ archive-initiatives.php
@@ -18,13 +18,13 @@ add_filter( 'get_the_archive_title', 'grd_custom_archive_title' );
  <div class="container mt-4 mb-5 pb-sm-4 ">
 	
  	<?php if ( have_posts() ): ?>
-	 	<h3 class="mt-4"> Current Task Forces </h3>
+	 	<h3 class="mt-4 heading-underline"> Current Task Forces </h3>
 		 <ul class="initiatives-list list-unstyled">
  		<?php while ( have_posts() ) : the_post(); ?>
 		 <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );?>
  		<article class="<?php echo $post->post_status; ?> post-list-item mt-4 mb-4 row  ">
 		
-			<li class=" py-5 Initiatives ">
+			<li class="pb-3 initiatives ">
  			<h2 class="h3 text-secondary">
  				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
  			</h2>

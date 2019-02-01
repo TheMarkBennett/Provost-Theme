32054858c29d48f30761ea25cf5ee1f6ed459ef5
Mark Bennett <mark.bennett@ucf.edu>
2019-02-01 10:47:12 -0500

    remove row tag

--- archive-initiatives.php
+++ archive-initiatives.php
@@ -25,7 +25,7 @@ add_filter( 'get_the_archive_title', 'grd_custom_archive_title' );
  		<article class="<?php echo $post->post_status; ?> post-list-item mt-4 mb-4 row  ">
 		
 			<li class="pb-3 initiatives ">
- 			<h2 class="h3 text-secondary row">
+ 			<h2 class="h3 text-secondary">
  				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
  			</h2>
  			<div class="summary">

534cf6b34e53811b4ef3b094457cc332f413f44a
Mark Bennett <mark.bennett@ucf.edu>
2019-02-20 16:22:44 -0500

    T

--- single-initiatives.php
+++ single-initiatives.php
@@ -4,39 +4,49 @@
 	<div class="container mt-3 mt-sm-2 mb-3 pb-sm-4">
 		<?php the_content(); ?>
 
-	<?php
+		<?php
 
-		// check if the repeater field has rows of data
-		if( have_rows('taskforce_tables') ): ?>
-			<div class="ucf_members">
+		// check for rows (parent repeater)
+				if( have_rows('taskforce_tables') ): ?>
+					<div class="members">
+					<?php 
 
-			<?php// loop through the rows of data
-			while ( have_rows('taskforce_tables') ) : the_row();?>
-
-			<div class="row">
-				<h2> <?php	the_sub_field('taskforce_section_title');?>	</h2>
-			</div>	
-
-				<?php if( have_rows('taskforce_section_members') ): ?>
-					<?php while ( have_rows('taskforce_section_members') ) : the_row(); ?>
+					// loop through rows (parent repeater)
+					while( have_rows('taskforce_tables') ): the_row(); ?>
 						<div class="row">
-							<div class="col-12 col-md-4">
-								<?php	the_sub_field('taskforce_member_name'); ?> 
-							</div>
-							<div class="col-12 col-md-4">
-								<?php	the_sub_field('taskforce_member_position'); ?>
-							</div>	
-							<div class="col-12 col-md-4">
-								<?php	the_sub_field('taskforce_member_email'); ?>
-							</div>	
-						</div>
-						<?php endwhile; ?>
-	
-				<?php endif; ?>
+							<h3><?php the_sub_field('taskforce_section_title'); ?></h3>
+						</div>	
+							<?php 
 
-			<?php endwhile; ?>
-			</div>		
-		<?php endif; ?>		
+							// check for rows (sub repeater)
+							if( have_rows('taskforce_section_members') ): ?>
+								
+								<?php 
+
+								// loop through rows (sub repeater)
+								while( have_rows('taskforce_section_members') ): the_row();
+
+									// display each item as a list - with a class of completed ( if completed )
+									?>
+									<div class="row">
+										<div class="col-12 col-md-4">
+											<?php the_sub_field('taskforce_member_name'); ?>
+										</div>
+										<div class="col-12 col-md-4">
+											<?php the_sub_field('taskforce_member_position'); ?>
+										</div>
+										<div class="col-12 col-md-4">
+											<?php the_sub_field('taskforce_member_email'); ?>
+										</div>
+									</div>
+								<?php endwhile; ?>
+								
+							<?php endif; //if( get_sub_field('items') ): ?>
+							
+
+					<?php endwhile; // while( has_sub_field('to-do_lists') ): ?>
+					</div>
+				<?php endif; // if( get_field('to-do_lists') ): ?>	
 	
 </article>
 

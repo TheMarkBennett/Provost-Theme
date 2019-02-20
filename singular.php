24048ba569394fefc37792cd4261f34df5ad8e79
Mark Bennett <mark.bennett@ucf.edu>
2019-02-20 11:26:59 -0500

    change padding

--- singular.php
+++ singular.php
@@ -1,7 +1,7 @@
 <?php get_header(); the_post(); ?>
 
 <article class="<?php echo $post->post_status; ?> post-list-item">
-	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
+	<div class="container mt-3 mt-sm-2 mb-3 pb-sm-4">
 		<?php the_content(); ?>
 	</div>
 </article>

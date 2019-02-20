9f8e11eac844c5de0d6b41991449549b720cb47d
Mark Bennett <mark.bennett@ucf.edu>
2019-02-20 13:53:56 -0500

    fix headers

--- archive-initiatives.php
+++ archive-initiatives.php
@@ -18,14 +18,14 @@ add_filter( 'get_the_archive_title', 'grd_custom_archive_title' );
  <div class="container mt-4 mb-5 pb-sm-4 ">
 	
  	<?php if ( have_posts() ): ?>
-	 	<h3 class="mt-4 heading-underline row"> Current Task Forces </h3>
+	 	<h2 class="mt-4 heading-underline row"> Current Task Forces </h3>
 		 <ul class="initiatives-list list-unstyled">
  		<?php while ( have_posts() ) : the_post(); ?>
 		 <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );?>
  		<article class="<?php echo $post->post_status; ?> post-list-item mt-4 mb-4 row  ">
 		
 			<li class="pb-3 initiatives ">
- 			<h2 class="h3 text-secondary">
+ 			<h3 class="text-secondary">
  				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
  			</h2>
  			<div class="summary">
@@ -40,7 +40,7 @@ add_filter( 'get_the_archive_title', 'grd_custom_archive_title' );
  		<p>No results found.</p>
  	<?php endif; ?>
 
-	 <h3 class="mt-4 heading-underline row"> Completed Task Forces </h3>
+	 <h2 class="mt-4 heading-underline row"> Completed Task Forces </h3>
  </div>
 
  <?php get_footer(); ?>

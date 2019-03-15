854f4e867b5dc41c9d253b1e1e160cd8fa7048c2
Mark Bennett <mark.bennett@ucf.edu>
2019-03-15 14:22:44 -0400

    added support for image

--- single.php
+++ single.php
@@ -1,9 +1,3 @@
 <?php get_header(); the_post(); ?>
-
-<article class="<?php echo $post->post_status; ?> post-list-item">
-	<div class="container mt-3 mt-sm-2 mb-3 pb-sm-4">
-        <?php get_template_part( 'template-parts/post/content', get_post_format() ); ?>
-	</div>
-</article>
-
+<?php get_template_part( 'template-parts/post/content', get_post_format() ); ?>
 <?php get_footer(); ?>

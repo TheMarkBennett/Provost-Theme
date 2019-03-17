56410882f32714941a87ce89af5b0bc5531ed269
Mark Bennett <mark.bennett@ucf.edu>
2019-03-17 14:31:04 -0400

    if

--- single.php
+++ single.php
@@ -1,3 +1,15 @@
-<?php get_header(); the_post(); ?>
+<?php
+
+if(is_singular('post') ):
+
+  get_header('newsroom');
+
+else:
+get_header();
+
+endif;
+
+
+the_post(); ?>
 <?php get_template_part( 'template-parts/post/content', get_post_format() ); ?>
 <?php get_footer(); ?>
